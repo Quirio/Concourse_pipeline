e1bbbcc044750ee050a0950bffbd1663e1b85ab8
Alejandro Hernandez Hernandez <quirio123@gmail.com>
2017-01-26 11:12:27 +0100

    test2

--- task/build.sh
+++ task/build.sh
@@ -3,6 +3,6 @@
 mkdir roles
 mkdir roles/docker
 ls 
-mv project-repository/* mv roles/docker/
+mv project-repository/* roles/docker/
 mv roles/tests/test.yml main.yml
 ansible-playbook project-repository/main.yml
